9e45d2f4a66b49eb41c4fcf79bbcb6ce6c91d1c2
refcell <abigger87@gmail.com>
2023-11-03 11:35:03 +0100

    set test dir to debug target

--- src/cli.rs
+++ src/cli.rs
@@ -162,8 +162,8 @@ For more information, try '--help'.
             .unwrap()
             .for_each(|e| println!("{:?}", e));
         let hoist_bin_dest = test_tempdir.join(HOIST_BIN);
-        std::fs::copy(hoist_bin, hoist_bin_dest).unwrap();
-        std::env::set_current_dir(&test_tempdir).unwrap();
+        std::fs::copy(hoist_bin, &hoist_bin_dest).unwrap();
+        std::env::set_current_dir(&PathBuf::from("target/debug")).unwrap();
         // list the contents of the test tempdir
         std::fs::read_dir(&test_tempdir)
             .unwrap()
